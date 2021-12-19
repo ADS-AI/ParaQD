3194ef5389a281d022afb90a0a7ffb751cbfe38a
rg089 <rishabhforprojects@gmail.com>
2021-12-19 18:48:06 +0530

    added numerical replacement

---
+++ operators/operations/replace_numerical_entities.py
@@ -0,0 +1,20 @@
+from .operation import Operation
+import numpy as np
+import wordtodigits
+import re
+
+
+class ReplaceNumericalEntities(Operation):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def generate(self, text, **kwargs):
+        possibilities = ["some", "a few", "many", "a lot of", ""]
+        replacement = np.random.choice(possibilities)
+        text = wordtodigits.convert(text)
+        numbers = list(set(re.findall("\d+\.?\d*", text)))
+        numbers = np.random.choice(numbers, np.random.randint(1, 3), replace=False)
+        for number in numbers:
+            text = re.sub(number, " "+replacement+" ", text)
+        text = re.sub("\s+", " ", text)
+        return text.strip()
