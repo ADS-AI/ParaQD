574381e5bc959fb3aafbe779b4d734abd1d098f7
rg089 <rishabhforprojects@gmail.com>
2021-12-20 22:22:59 +0530

    updated tfidf replacement

--- operators/operations/tfidf_replacement.py
+++ operators/operations/tfidf_replacement.py
@@ -2,18 +2,23 @@ from .operation import Operation
 import numpy as np
 import pickle
 import random
+import os
 
 
 class TF_IDF_Replacement(Operation):
-    def __init__(self, tfidf_path) -> None:
+    def __init__(self) -> None:
         super().__init__()
+        resource_dir = os.path.join(os.path.dirname(__file__), "..", "resources")
+        tfidf_path = os.path.join(resource_dir, "tfidf_aqua.pkl")
         self.tfidf = pickle.load(open(tfidf_path, "rb"))
         self.words = self.tfidf.get_feature_names()
 
     def __sample(self, n=3):
         return random.sample(self.words, n)
 
-    def generate(self, text, soften=False, **kwargs):
+    def generate(self, text, **kwargs):
+        soften = kwargs.get("soften", False)
+        
         transformed = self.tfidf.transform([text]).toarray()
         most_imp = np.argpartition(transformed, -4)[:, -4:]
         array = most_imp[0]
