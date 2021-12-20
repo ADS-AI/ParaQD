ffbf98c9cd32cd2e622ea365413eb43656d6a3ee
rg089 <rishabhforprojects@gmail.com>
2021-12-20 23:33:57 +0530

    added data generator

---
+++ operators/operations.py
@@ -0,0 +1,15 @@
+from .positive_ops import PositiveSamples
+from .negative_ops import NegativeSamples
+
+
+class GenerateData(object):
+    def __init__(self):
+        self.positive_generator = PositiveSamples()
+        self.negative_generator = NegativeSamples()
+
+    def generate(self, text, positive_ops=["BackTranslate", "SameSentence", "Num2Words", "UnitExpansion"], 
+                negative_ops=["MostImportantPhraseRemover", "DeleteLastSentence", "ReplaceNamedEntities",
+                            "ReplaceNumericalEntities", "Pegasus", "ReplaceUnits", "NegateQuestion"]):
+        positives = self.positive_generator.generate(text, ops=positive_ops)
+        negatives = self.negative_generator.generate(text, ops=negative_ops)
+        return positives, negatives
