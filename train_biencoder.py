083ba74af97aae9e32428c580bb204d790947129
rg089 <rishabhforprojects@gmail.com>
2021-12-31 19:54:01 +0530

    added seeding

--- train_biencoder.py
+++ train_biencoder.py
@@ -7,6 +7,8 @@ import os
 import numpy as np
 import random
 import torch
+import math
+import json
 
 
 def set_seed(seed):
@@ -81,7 +83,75 @@ def generate_samples(df, anchor_column="question", positive_cols=[], negative_co
             train_samples.append(InputExample(texts = [anchor_text, positive, negative]))
 
     return train_samples
+
+
+def train_with_seed(seed, train_samples, val_samples, model_path='sentence-transformers/paraphrase-mpnet-base-v2', stop_after = 0.2, 
+            num_epochs=1, batch_size=16, output_dir='output', verbose=True, save_loss=False, loss_fn="triplet"):
+    """
+    Trains for one epoch with stop_after fraction of the training data for a given seed
+    """
+    set_seed(seed)
+
+    model = SentenceTransformer(model_path)
+    train_dataset = DataLoader(train_samples, batch_size=batch_size, shuffle=True)
+    evaluator = evaluation.TripletEvaluator.from_input_examples(val_samples)
+
+    warmup_steps = int(len(train_dataset) * num_epochs * 0.1)
+    steps_per_epoch = math.ceil( len(train_dataset) / batch_size * stop_after)
+
+    if loss_fn == "triplet":
+        train_loss = losses.TripletLoss(model=model)
+    elif loss_fn == "mnrl":
+        train_loss = losses.MultipleNegativesRankingLoss(model=model)
+    else:
+        raise NotImplementedError
+
+    model_save_path = os.path.join(output_dir, 'seed_{}'.format(seed))
+
+    model.fit(train_objectives=[(train_dataset, train_loss)],
+        evaluator=evaluator,
+        epochs=num_epochs,
+        evaluation_steps=150,
+        steps_per_epoch=steps_per_epoch,
+        warmup_steps=warmup_steps,
+        output_path=model_save_path)
+
+    if verbose:
+        print(f"Seed {seed} finished!")
+
+
+def read_eval_file(eval_path):
+    """
+    reads the eval file from the evaluation path
+    returns the mean of the last three cosine accuracies
+    """
+    eval_file = [f for f in os.listdir(eval_path) if f.endswith('.csv')][0]
+    eval_df = pd.read_csv(os.path.join(eval_path, eval_file))
+    cosine_accuracies = eval_df["accuracy_cosinus"].values
+    # Take the mean of the last three values of the Series
+    cosine_acc = np.mean(cosine_accuracies[-3:])
+    return cosine_acc
+
+
+def get_best_seed(model_dir):
+    """
+    given the model dir, returns the best seed by reading the seed results
+    """
+
+    model_paths = [os.path.join(model_dir, f) for f in os.listdir(model_dir) if f.startswith('seed_')]
+    seeds = [int(f.split('_')[-1].split('.')[0]) for f in model_paths]
+    eval_paths = [os.path.join(model_path, "eval") for model_path in model_paths]
+
+    best_seed = -1; cosine_acc = -1
+    for seed, eval_path in zip(seeds, eval_paths):
+        cosine_results = read_eval_file(eval_path)
+
+        if cosine_results > cosine_acc:
+            best_seed = seed
+            cosine_acc = cosine_results
     
+    return best_seed
+
 
 def train(train_samples, val_samples, model_path='sentence-transformers/paraphrase-mpnet-base-v2', num_epochs=10, 
         batch_size=16, output_dir='output', verbose=True, save_loss=False, loss_fn="triplet"):
@@ -128,13 +198,11 @@ def train(train_samples, val_samples, model_path='sentence-transformers/paraphra
         print(f"[INFO] Training finished!")
         print(f"[INFO] Saving model to {output_dir}")
 
-    model.save(output_dir)
     if save_loss:
-        train_loss.save(os.path.join(output_dir, 'loss'))
+        torch.save(train_loss, os.path.join(output_dir, 'loss', 'pytorch_loss.bin'))
 
 
 if __name__ == '__main__':
-    set_seed(3407)
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--train_path", "-t", type=str, default="data/train.csv", help="path to the training data")
@@ -152,22 +220,57 @@ if __name__ == '__main__':
     parser.add_argument("--max_triplets_per_sample", "-mt", type=int, default=-1, help="the maximum number of triplets per sample")
     parser.add_argument("--save_loss", "-sl", action="store_true", help="if true, the loss is saved")
     parser.add_argument("--loss_fn", "-lf", type=str, default="triplet", help="the loss function to use")
+    parser.add_argument("--seed", "-s", type=int, default=-1, help="the random seed")
+    parser.add_argument("--num_seeds", "-ns", type=int, default=-1, help="the number of seeds")
+    parser.add_argument("--stop_after", "-sa", type=int, default=0.2, help="the amount of batches to stop after")
+    parser.add_argument("--continue_training", "-ct", action="store_true", help="if true, the training with best seed is continued")
 
     args = parser.parse_args()
+
+    if args.seed == -1 and args.num_seeds == -1:
+        num_seed = 5
+    elif args.seed != -1 and args.num_seeds != -1:
+        raise ValueError("You can either specify a seed or the number of seeds, not both")
+    
+    seed_list = [args.seed] if args.seed != -1 else [i for i in range(args.num_seeds)]
+    
 
     train_df = pd.read_csv(args.train_path)
     val_df = pd.read_csv(args.val_path)
 
-    train_samples = generate_samples(df=train_df, anchor_column=args.anchor_column, positive_cols=args.positive_cols, 
-                                    negative_cols=args.negative_cols, use_inbatch=args.use_inbatch, 
-                                    max_triplets_per_sample=args.max_triplets_per_sample)
+    for seed in seed_list:
+        set_seed(seed)
 
-    val_samples = generate_samples(df=val_df, anchor_column=args.anchor_column, positive_cols=args.positive_cols,
-                                    negative_cols=args.negative_cols, use_inbatch=args.use_inbatch,
-                                    max_triplets_per_sample=args.max_triplets_per_sample)
+        train_samples = generate_samples(df=train_df, anchor_column=args.anchor_column, positive_cols=args.positive_cols, 
+                                        negative_cols=args.negative_cols, use_inbatch=args.use_inbatch, 
+                                        max_triplets_per_sample=args.max_triplets_per_sample)
 
-    model = train(train_samples=train_samples, val_samples=val_samples, model_path=args.model_path, num_epochs=args.num_epochs,
-                batch_size=args.batch_size, output_dir=args.output_dir, verbose=args.verbose, save_loss=args.save_loss, loss_fn=args.loss_fn)
+        val_samples = generate_samples(df=val_df, anchor_column=args.anchor_column, positive_cols=args.positive_cols,
+                                        negative_cols=args.negative_cols, use_inbatch=args.use_inbatch,
+                                        max_triplets_per_sample=args.max_triplets_per_sample)
+
+        model = train_with_seed(seed=seed, train_samples=train_samples, val_samples=val_samples, model_path=args.model_path, num_epochs=1,
+                    batch_size=args.batch_size, output_dir=args.output_dir, verbose=args.verbose, save_loss=args.save_loss, 
+                    loss_fn=args.loss_fn, stop_after=args.stop_after)
+
+    best_seed = get_best_seed(output_dir=args.output_dir)
+    with open(os.path.join(args.output_dir, 'best_seed.json'), 'w') as f:
+        info = {'best_seed': best_seed}
+        json.dump(info, f)
+
+    if args.continue_training:
+        set_seed(best_seed)
+        train_samples = generate_samples(df=train_df, anchor_column=args.anchor_column, positive_cols=args.positive_cols, 
+                                            negative_cols=args.negative_cols, use_inbatch=args.use_inbatch, 
+                                            max_triplets_per_sample=args.max_triplets_per_sample)
+
+        val_samples = generate_samples(df=val_df, anchor_column=args.anchor_column, positive_cols=args.positive_cols,
+                                            negative_cols=args.negative_cols, use_inbatch=args.use_inbatch,
+                                            max_triplets_per_sample=args.max_triplets_per_sample)
+
+        model = train(train_samples=train_samples, val_samples=val_samples, model_path=args.model_path, num_epochs=args.num_epochs,
+                        batch_size=args.batch_size, output_dir=args.output_dir, verbose=args.verbose, save_loss=args.save_loss, 
+                        loss_fn=args.loss_fn)
 
     """
     To train the model, run the following command:
