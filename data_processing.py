8aed494be531c47c73675ec381396ddc9171316d
rg089 <rishabhforprojects@gmail.com>
2022-01-03 16:30:41 +0530

    updated raw generation code

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import re
 import numpy as np
+import argparse
+import os
 
 
 def create_raw_versions(fpath, columns=["question"], dataset_name="AQUA", split="train"):
@@ -13,11 +15,19 @@ def create_raw_versions(fpath, columns=["question"], dataset_name="AQUA", split=
     else: df = pd.read_excel(fpath)
 
     raw_df = df.loc[:, columns]
-    output_fname = f"data/{dataset_name}_{split}_raw.csv"
+    output_fname = os.path.join(os.getcwd(), "data", dataset_name, f"{dataset_name}_{split}_raw.csv")
     raw_df.to_csv(output_fname, index=False)
 
 
 
 if __name__ == "__main__":
-    create_raw_versions("D:/Material/NLP/NLP Projects/ParaQD\ParaQD/val_samples_augmented_aqua_21821_22K.csv",
-                         dataset_name="AQUA", split="val")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--fpath", type=str, required=True)
+    parser.add_argument("--dataset", type=str, default="AQUA", help="Dataset name")
+    parser.add_argument("--split", type=str, default="train", help="Dataset split")
+    parser.add_argument("--columns", nargs="+", type=str, default=["question"], help="Columns to use")
+
+    args = parser.parse_args()
+
+    create_raw_versions(args.fpath, columns=args.columns, dataset_name=args.dataset, split=args.split)
